6ec0ffe96ca4e502fafffcfba449b77a97172d8e
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-05-25 00:25:09 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (40.98%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int ans=0;
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='R'){
+                ans+=-1;
+            }else{
+                ans+=1;
+            }
+            
+            if(ans==0)
+                count++;
+        }
+        
+        
+        
+        return count;
+    }
+};
