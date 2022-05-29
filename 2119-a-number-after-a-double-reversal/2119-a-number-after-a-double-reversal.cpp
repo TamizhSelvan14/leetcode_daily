0e1e4aac04840c0ecc18e0ea0272239db94c5072
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-05-30 00:04:55 +0530

    Time: 5 ms (10.11%), Space: 6 MB (22.22%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        if(num==0)
-            return true;
+//         if(num==0)
+//             return true;
         
-        if((num%10) ==0)
-            return false;
+//         if((num%10) ==0)
+//             return false;
     
         
-        return true;
+//         return true;
+        
+        //same above in one line
+        
+        return (num==0) || (num%10)!=0;
+        
     }
 };
