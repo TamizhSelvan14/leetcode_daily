53c9927b8e03f1ce6b3828cd83ef3176d698851f
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-05-31 23:15:52 +0530

    Time: 0 ms (100.00%), Space: 7 MB (73.59%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n=matrix.size();
+        
+//         map<int,int> hashmap;
+        
+//         int temp=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+                
+//                 hashmap.insert({temp,matrix[i][j]});
+//                 temp++;
+                
+//             }
+            
+//         }
+        
+//         int var=n-1;
+//         auto iter = hashmap.begin();
+        
+       
+//         for(int i=0;i<n;i++){
+            
+//             for(int j=0;j<n;j++){
+                
+//                 matrix[j][var]=iter->second;
+//                  iter++;
+                
+//             }
+//             var--;
+//         }
+        
+        
+        for(int i=0;i<n;i++){
+					
+			for(int j=0;j<i;j++){
+					swap(matrix[i][j] , matrix[j][i]);
+				
+					}
+			}
+					
+					for(int i=0;i<n;i++){
+					reverse(matrix[i].begin(),matrix[i].end());
+					}
+        
+        
+    }
+};
