c38fe38a05f6577af1842d1e2c5e18390ab6cdd6
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-06-02 09:34:28 +0530

    Time: 44 ms (7.80%), Space: 6.2 MB (88.17%) - LeetHub

---
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int n) {
+        vector<int> ans(n,0);
+        
+      int temp=1;
+            int i=0;
+            while(candies!=0){
+      
+                    if( (temp) <candies){
+                        ans[i%n]+=temp;
+                        candies-=temp;
+                        cout<<temp<<" ";
+                        temp++;
+                        i++;
+                    }else{
+                        ans[i%n]+=candies;
+                        candies=0;
+                        i++;
+                    }
+                
+        }
+        
+        
+        return ans;
+    }
+};
