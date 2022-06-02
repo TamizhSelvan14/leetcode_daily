2a8c8eebd0beba773a0a2cea241850612e92b281
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-06-03 00:05:10 +0530

    Time: 43 ms (88.63%), Space: 16 MB (52.98%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    // struct box {
+    // int start, end;
+    // };
+
+    
+ 
+    
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(),boxTypes.end(),[](auto& a, auto& b){ return a[1] > b[1];});
+        
+        int ans=0;
+        for(int i=0;i<boxTypes.size();i++){
+            
+            if(truckSize-boxTypes[i][0]>=0){
+                
+                ans+=boxTypes[i][0]*boxTypes[i][1];
+                truckSize-=boxTypes[i][0];
+            }else{
+                ans+=truckSize*boxTypes[i][1];
+                break;
+            }
+            
+            
+        }
+        
+        
+        return ans;
+    }
+};
