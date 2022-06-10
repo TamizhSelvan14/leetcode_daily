bbf1baae6c0a112dbeba4c0c3a0431a62fb30089
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-06-10 09:18:05 +0530

    Time: 12 ms (79.71%), Space: 8.4 MB (59.36%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    //THIS FUNCTION CHECK IS THE CHARACTER REPEATS THEN RETURNS FALSE
-//     bool distinct(string s,int i,int j){
-        
-//         vector<bool> visited (26);
-        
-//         for( int k=i;k<=j;k++){
-//             if(visited[s[k]-'a']==true){
-//                 return false;
-//             }
-//             visited[s[k]-'a']=true;
-//         }
-        
-//         return true;
-//     }
-    
-    
-    
-    
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-    int res = 0; // result
+        
+        unordered_map<int,int> umap;
+        
+        int n=s.size();
         int start=0;
-    unordered_map<char,int> umap;
-    for (int i = 0; i < n; i++) {
-         
-        if(umap.find(s[i])!=umap.end() and umap[s[i]]>=start)
-        {
-            start=umap[s[i]]+1;
-        }
         
-        umap[s[i]]=i;
-        res=max(res,i-start+1);
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            
+            
+                    if(umap.find(s[i])!=umap.end() and umap[s[i]]>=start){
+                        ///if we found an element move start at umap[i]+1
+                        
+                        start=umap[s[i]]+1;
+                    }
+            
+                    umap[s[i]]=i;
+                    maxi=max(maxi,i-start+1);
+            // cout<<maxi<<endl;
+            }
+            
         
-        
-    }
-    return res;
+        return maxi;
     }
 };
