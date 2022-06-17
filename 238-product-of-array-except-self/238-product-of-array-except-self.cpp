8edbd6bca2cc7777796e0216725ea101d60b0c8a
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-06-17 17:41:41 +0530

    Time: 58 ms (7.44%), Space: 23.9 MB (81.71%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -7,32 +7,53 @@ public:
         
         vector<int> ans(n);
         
-        vector<int> prefixProduct(n);
-        vector<int> suffixProduct(n);
+//         //following cosde has space of 0(N)
+//         vector<int> prefixProduct(n);
+//         vector<int> suffixProduct(n);
+//         int pre=1;
+//         for(int i=0;i<n;i++){
+            
+//             prefixProduct[i]=nums[i]*pre;
+//             pre=prefixProduct[i];
+            
+//         }
+        
+//         int suff=1;
+//         for(int i=n-1;i>=0;i--){
+            
+//             suffixProduct[i]=nums[i]*suff;
+//             suff=suffixProduct[i];
+            
+//         }
+        
+//         ans[0]=suffixProduct[1];
+//         ans[n-1]=prefixProduct[n-2];
+            
+            
+            
+//         for(int i=1;i<n-1;i++){
+//             ans[i]=prefixProduct[i-1]*suffixProduct[i+1];
+//         }
+        
+        
         int pre=1;
-        for(int i=0;i<n;i++){
-            
-            prefixProduct[i]=nums[i]*pre;
-            pre=prefixProduct[i];
-            
+        
+        for(int i=0;i<n;i++)
+        {
+            ans[i]=pre;
+            pre=nums[i]*pre;
         }
         
         int suff=1;
-        for(int i=n-1;i>=0;i--){
-            
-            suffixProduct[i]=nums[i]*suff;
-            suff=suffixProduct[i];
-            
+        for(int i=n-1;i>=0;i--)
+        {
+            ans[i]*=suff;
+            suff=nums[i]*suff;
         }
         
-        ans[0]=suffixProduct[1];
-        ans[n-1]=prefixProduct[n-2];
-            
-            
-            
-        for(int i=1;i<n-1;i++){
-            ans[i]=prefixProduct[i-1]*suffixProduct[i+1];
-        }
+        
+        
+        
         
         
         return ans;
