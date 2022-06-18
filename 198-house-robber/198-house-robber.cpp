75428fde4ebe8ef87667c48fe531fba7ba3193b8
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-06-18 23:30:29 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (75.54%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -17,9 +17,31 @@ public:
     int rob(vector<int>& nums) {
         
         int n=nums.size();
-        vector<int> dp(n,-1);
-        // cout<<ans(nums,n-1)<<"  "<<ans(nums,n-2);
-        return max(ans(nums,n-1,dp),ans(nums,n-2,dp));
+        vector<int> dp(n,0);
+        
+        if(nums.size()==1)
+            return nums[0];
+        else if(n==2)
+            return max(nums[0],nums[1]);
+        
+        
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+
+                for(int i=2;i<n;i++){
+            
+                        dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            
+                }
+        
+        
+        
+        // int t=max(ans(nums,n-1,dp),ans(nums,n-2,dp));
+        
+//         for(auto x:dp)
+//             cout<<x<<" ";
+        
+        return dp[n-1];
         
         
     }
