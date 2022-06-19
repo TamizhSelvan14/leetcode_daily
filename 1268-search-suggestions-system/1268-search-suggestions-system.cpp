17b010de35700490861f2f076f9ae15dd0bb014d
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-06-19 16:12:02 +0530

    Time: 625 ms (17.18%), Space: 182.9 MB (14.06%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
+    
+    // return rows with size of sub problem so take a empty string and traverse at each time add the next vhar to the string S then traverse products make the first prefix of each word to the size of S and compare if its same push to the vector
+    
+    
+    
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
         vector<vector<string>> ans;
         string s="";
         
+        ///sorting
         sort(products.begin(),products.end());
         
+        //traverse the string searchWord
         for(int i=0;i<searchWord.size();i++){
-            vector<string> sub;
-            s+=searchWord[i];
-            // cout<<s<<endl;
-            int k=0;
+            vector<string> sub; //temp arr to push 
+            s+=searchWord[i];  //each time add new element
+          
+            //traverse products
             for(int j=0;j<products.size();j++){
                 
+                //make the products[i] string size as same as S 
                 string t=products[j].substr(0,s.size());
-                // cout<<t<<endl;
                 
-                
+                //compare if same push to the vector make sure it doesnt exceed size of 3
                 if(s==t and sub.size()<3){
                     sub.push_back(products[j]);
-                    // k++;
                 }
                 
             }
             ans.push_back(sub);
         }
-        
         
         
         return ans;
