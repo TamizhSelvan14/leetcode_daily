e8ebee29c844a5351467b8ee96de7cdd09156165
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-06-21 00:12:51 +0530

    Time: 18 ms (31.39%), Space: 13.9 MB (80.39%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,57 +1,50 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&image,int sr,int sc,int row,int col,int newColor,int orgColor){
+    
+    void solve(vector<vector<int>> &image,int i,int j,int presentColor,int desiredColor){
         
         
-        //check boundaries
-        if(sr<0 || sc<0 || sr>=row || sc>=col)
-            return;
-        else if(image[sr][sc]!=orgColor)
-            return;
-        
-        
-        
-        
-        //traverse all sides
-        image[sr][sc]=newColor;
-        
-        
-        dfs(image,sr+1,sc,row,col,newColor,orgColor);
-        
-        dfs(image,sr,sc+1,row,col,newColor,orgColor);
-        
-        dfs(image,sr-1,sc,row,col,newColor,orgColor);
-        
-        dfs(image,sr,sc-1,row,col,newColor,orgColor);
-        
-       
+        if(i>=0 and i<image.size() and j>=0 and j<image[0].size() and image[i][j]==presentColor){
+            
+            if(image[i][j]==desiredColor)
+                return;
+            
+            
+            image[i][j]=desiredColor;
          
-        
-        // return;
+            
+            solve(image,i+1,j,presentColor,desiredColor);
+            
+            solve(image,i,j+1,presentColor,desiredColor);
+            
+            solve(image,i-1,j,presentColor,desiredColor);
+            
+            solve(image,i,j-1,presentColor,desiredColor);
+            
+            
+            
+            
+        }
     }
     
     
     
     
     
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
-        //if its already in same color return the graph
-        if(image[sr][sc]==newColor)
-            return image;
+       
         
+        int row=image.size();
+        int col=image[0].size();
         
-        int row=size(image);
-        int col=size(image[0]);
-        int orgColor=image[sr][sc];
+        int presentColor=image[sr][sc];
         
         
-        dfs(image,sr,sc,row,col,newColor,orgColor);
+     solve(image,sr,sc,presentColor,color);
+        
         
         
         return image;
-        
-        
-        
     }
 };
