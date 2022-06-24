72813e3e333a66a3dda06740c00a968b4d879260
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-06-24 20:28:32 +0530

    Time: 308 ms (42.26%), Space: 8.2 MB (66.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,51 +2,52 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         
+        int maxlength=1;
+        int start=0;
+        
         int n=s.size();
         
         bool dp[n][n];
         
         memset(dp,0,sizeof(dp));
         
-        int start=0;
-        int maxlength=1;//because evry element is palindrome
         
-        //1 char palindrome
-        for(int i=0;i<n;i++)
-            dp[i][i]=true;
         
-        // 2 char palindrome
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<s.size();i++){
+            
+            dp[i][i]=1;
+           
+        }
+        
+        
+        for(int i=0;i<s.size()-1;i++){
+            
             if(s[i]==s[i+1]){
-                dp[i][i+1]=true;
+                dp[i][i+1]=1;
                 start=i;
-                maxlength=2;    
+                maxlength=2;
             }
+            
         }
         
         
-        //for three char where k will be on centree and the i j traverse from alst
-        //i from front j from last
-        
-        for(int k=3;k<=n;k++){
-            for(int i=0;i<n-k+1;i++){
-                int j=i+k-1;
+        for(int j=2;j<n;j++){
+            for(int i=0;i<n-j;i++){
                 
-                if(dp[i+1][j-1] and s[i]==s[j])
-                {
-                    dp[i][j]=true;
+                int left=i;
+                int right=i+j;
+                
+                if(dp[left+1][right-1]==1 and s[left]==s[right]){
+                    dp[left][right]=1;
                     start=i;
-                    maxlength=max(maxlength,k);
+                    maxlength=j+1;
                 }
+                
             }
         }
-       
         
-        // string str="";
-        // for(int i=start;i<start+maxlength;i++)
-        //     str+=s[i];
-         // cout<<start<<" "<<maxlength<<endl;
-      return s.substr(start, maxlength);
         
+        
+        return s.substr(start,maxlength);
     }
 };
