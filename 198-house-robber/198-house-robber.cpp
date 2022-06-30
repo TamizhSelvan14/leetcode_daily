8a6da9a221cccca28e6bdb20338d64cac5f80e8c
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-06-30 22:46:40 +0530

    Time: 7 ms (11.72%), Space: 7.9 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    
-    
     int solve(vector<int> &nums,int n,vector<int> &dp){
-        
         if(n<0)
             return 0;
         
-        // if(n==0 or n==1){
-        //     return nums[n];
-        // }
+        
         if(dp[n]!=-1)
             return dp[n];
         
-        int a=nums[n]+solve(nums,n-2,dp);
-        int b=solve(nums,n-1,dp);
-        
-        
-        
-        return dp[n]=max(a,b);
-        
-        
+        return dp[n]=max(nums[n]+solve(nums,n-2,dp),solve(nums,n-1,dp));
         
         
     }
     
-    
-    
     int rob(vector<int>& nums) {
-        vector<int> dp (1001,-1);
-      return  solve(nums,nums.size()-1,dp);
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return solve(nums,n-1,dp);
+        
+        
     }
 };
