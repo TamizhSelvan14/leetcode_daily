0cb4ea94d98b990daa2774535dc6194372c03b3c
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-03 22:32:26 +0530

    Time: 20 ms (60.68%), Space: 13.3 MB (5.49%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -33,33 +33,32 @@ public:
         
         int mini=INT_MAX;
         vector<vector<int>> dp(n,vector<int>(n,0));
-        //traversing the firt col
-        for(int i=0;i<n;i++){
+        
+//         //traversing the firt col
+//         for(int i=0;i<n;i++){
             
-        dp[0][i]=matrix[0][i];
+//         dp[0][i]=matrix[0][i];
             
-        }
+//         }
         
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
                 
-                if(j==0){
-                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j+1]);
-                }else if(j==n-1)
-                   dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j-1]);
-                else{
-                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],min(dp[i-1][j-1],dp[i-1][j+1]));
+                if(j==0){// 3.1->left most row
+                    matrix[i][j]+=min(matrix[i-1][j],matrix[i-1][j+1]);
+                }else if(j==n-1)//3.2 right most row
+                   matrix[i][j]+=min(matrix[i-1][j],matrix[i-1][j-1]);
+                else{//rest
+                    matrix[i][j]+=min(matrix[i-1][j],min(matrix[i-1][j-1],matrix[i-1][j+1]));
                     
                 }
-                
+                //last col max
+               
                 
             }
         }
         
-        for(int i=0;i<n;i++)
-            mini=min(mini,dp[n-1][i]);
-        
-        
+        mini=*min_element(matrix[n-1].begin(),matrix[n-1].end());
         return mini;
     }
 };
