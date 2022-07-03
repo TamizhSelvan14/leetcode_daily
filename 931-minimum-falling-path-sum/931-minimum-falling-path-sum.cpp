ea5970148a7bac9ec7a53caf417924b4d257d603
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-03 21:30:22 +0530

    Time: 42 ms (6.11%), Space: 13.2 MB (5.49%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -32,13 +32,32 @@ public:
         int n=matrix.size();
         
         int mini=INT_MAX;
-        
+        vector<vector<int>> dp(n,vector<int>(n,0));
         //traversing the firt col
         for(int i=0;i<n;i++){
             
-        mini=min(mini ,solve(0,i,matrix));
+        dp[0][i]=matrix[0][i];
             
         }
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                
+                if(j==0){
+                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j+1]);
+                }else if(j==n-1)
+                   dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j-1]);
+                else{
+                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],min(dp[i-1][j-1],dp[i-1][j+1]));
+                    
+                }
+                
+                
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+            mini=min(mini,dp[n-1][i]);
         
         
         return mini;
