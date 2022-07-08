5458b17d96667882b9d14043c69444a9b8c3980d
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-08 15:03:25 +0530

    Time: 305 ms (8.48%), Space: 49.4 MB (11.98%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -53,11 +53,61 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
         
-       int ans= solve(n-1,coins,amount,dp);
+//        int ans= solve(n-1,coins,amount,dp);
         
-        return (ans==1e9) ? -1 :ans;
+//         return (ans==1e9) ? -1 :ans;
+        
+        
+        for(int i=0;i<=n;i++)
+        {
+            
+        for(int j=0;j<=amount;j++)
+            {
+            
+            if(j==0)
+            dp[i][j]=0;
+            else
+                dp[i][j]=INT_MAX-1;
+            
+            
+            }       
+        }
+        
+        
+        
+        if(amount>=coins[0])
+        dp[0][coins[0]]=1;
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+               
+                if (coins[i-1 ] > j)
+                    dp[i][j] = 0 + dp[i - 1][j];
+                else
+                    dp[i][j] = min(0 + dp[i - 1][j], 1 + dp[i][j - coins[i-1 ]]);
+                
+            }
+        }
+        
+        
+//          for(int i=0;i<=n;i++){
+//             for(int j=0;j<amount+1;j++){
+              
+//                 cout<<dp[i][j]<<" ";
+                
+//             }
+//              cout<<endl;
+//         }
+        
+        
+        
+        int ans= (dp[n][amount]) ;
+        
+        
+        return (ans==INT_MAX-1) ? -1:ans;
         
     }
 };
