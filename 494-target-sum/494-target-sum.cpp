339f45505aa02a60a61043eac4410dce0f0fd4b0
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-08 18:01:32 +0530

    Time: 1910 ms (6.15%), Space: 390 MB (5.01%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    int solve(vector<int>& nums, int i,int target, vector<vector<int>> &dp){
+        
+        if(i<0)
+            return 0;
+        
+        
+        if(i==0){
+            
+            // if(target==0 and  (  nums[i]*(-1)==0 or nums[i]*(+1)==0  ) )
+            //    return 1;
+            
+            if(((nums[i]*(-1))) ==target){
+                return 1;
+            }
+            
+            
+            if(((nums[i]*(+1) )) == target){
+                return 1;
+            }
+            
+            return 0;
+        }
+        
+        
+    // if(dp[i][curr]!=-1)
+        // return dp[i][curr];
+        
+        int negative=solve(nums,i-1,target + (nums[i] * (-1)) , dp );
+        int positive=solve(nums,i-1,target + (nums[i] * (+1)) ,  dp );
+        
+        
+        
+        return negative+positive;
+        
+        
+    }
+    
+    
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>> dp(1001,vector<int>(10001,-1));
+       int ans= solve(nums,n-1,target,dp);
+        
+        return nums[0]==0 ? ans*2 :ans;
+        
+        
+    }
+};
