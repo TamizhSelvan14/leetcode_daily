ad328924a1ca3c6b1368d450deef4f7efaff52c9
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-17 16:05:45 +0530

    Time: 51 ms (62.60%), Space: 18.5 MB (47.60%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -3,13 +3,14 @@ public:
     
     int solve(int i,int j,string &s,string &t,vector<vector<int>> &dp){
         
-        
+        //base 1
         if(i==0 and j==0)
             return 1;
-        
+        //base 2
         if(i==0)
             return 0;
         
+        //base 3
         if(j==0)
             return 1;
         
@@ -32,8 +33,30 @@ public:
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         
-        return solve(n,m,s,t,dp);
+        int mod =1e9+7;
+        
+        dp[0][0]=1;
+      
+        //base 3
+        for(int i=0;i<=n;i++)
+            dp[i][0]=1;
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                
+                if(s[i-1]==t[j-1])
+                    dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%mod;
+                else
+                    dp[i][j]=dp[i-1][j];
+                
+                
+            }
+        }
+        
+        return dp[n][m];
+        // return solve(n,m,s,t,dp);
     }
 };
