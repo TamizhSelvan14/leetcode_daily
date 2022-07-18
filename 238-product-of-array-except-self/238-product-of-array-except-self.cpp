5ba58c5bfb9b893725bd9b793a4f40e3447b268d
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-18 12:10:34 +0530

    Time: 39 ms (49.25%), Space: 27.4 MB (7.14%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -4,41 +4,34 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
         int n=nums.size();
-        vector<int> preProduct;
+        vector<int> preProduct(n+1);
         
         vector<int> suffProduct(n+1);
         
-        preProduct.push_back(1);
+        preProduct[0]=(1);
         
-        suffProduct.push_back(1);
+        suffProduct[0]=(1);
         
         int pre=1;
         int post=1;
-        for(int i=0;i<nums.size();i++){
+        
+        int i=0;int j=n-1;
+       while(i<n-1 and j>0){
             
             int val1=pre*nums[i];
             pre=val1;
             
-            preProduct.push_back(val1);
+            preProduct[i+1]=(val1);
             
-            
-            
-            
-        }
-        
-        for(int j=n-1;j>=0;j--){
-            
-            int val2=post*nums[j];
+             int val2=post*nums[j];
             post=val2;
             
             suffProduct[j+1]=(val2);
         
+            i++;j--;
             
         }
-        suffProduct[0]=1;
-        // for(auto x:suffProduct){
-        //     cout<<x<<" ";
-        // }
+       
         
         
         preProduct.push_back(1);
