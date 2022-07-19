013d11893b7d328b63bc6cae2533a9b97b25e700
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-19 13:21:45 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.20%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,47 +3,18 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         
         int n=matrix.size();
-        
-//         map<int,int> hashmap;
-        
-//         int temp=0;
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<n;j++){
-                
-//                 hashmap.insert({temp,matrix[i][j]});
-//                 temp++;
-                
-//             }
-            
-//         }
-        
-//         int var=n-1;
-//         auto iter = hashmap.begin();
-        
-       
-//         for(int i=0;i<n;i++){
-            
-//             for(int j=0;j<n;j++){
-                
-//                 matrix[j][var]=iter->second;
-//                  iter++;
-                
-//             }
-//             var--;
-//         }
-        
-        //transpose  of a matrix
+        //TRANPOSE
         for(int i=0;i<n;i++){
-			for(int j=0;j<i;j++){
-					swap(matrix[i][j],matrix[j][i]);
-				}
-		}
-					
-			
-        //reversing to get the ans
-            for(int i=0;i<n;i++){
-				reverse(matrix[i].begin(),matrix[i].end());
-			}
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        
+        //reverse every row
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
         
         
     }
