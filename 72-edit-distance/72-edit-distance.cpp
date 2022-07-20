6a4ab017111b1280cdfe717bc0fa5b810f7cbb4a
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-20 17:27:15 +0530

    Time: 518 ms (5.00%), Space: 10.5 MB (16.40%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -34,9 +34,43 @@ public:
         int n=word1.size();
         int m=word2.size();
         
-        memset(dp,-1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
         
-        return solve(n-1,m-1,word1,word2);
+        
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        
+        for(int i=0;i<=m;i++){
+            dp[0][i]=i;
+        }
+        
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                 
+                if(word1[i-1]==word2[j-1]){
+                 dp[i][j]=dp[i-1][j-1];
+            
+                }else{
+
+                   int insert=dp[i-1][j];
+
+                   int deletion=dp[i][j-1];
+                   int replace=dp[i-1][j-1];
+
+                     dp[i][j]= 1+min(insert,min(deletion,replace));
+
+                }
+            }
+        }
+        
+        
+        
+        return dp[n][m];
+        // return solve(n-1,m-1,word1,word2);
         
     }
 };
