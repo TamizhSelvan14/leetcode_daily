f9499cb3ebe6a833c41411f671731d5050bd58da
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-23 16:21:31 +0530

    Time: 66 ms (21.42%), Space: 28.3 MB (20.91%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -5,21 +5,23 @@ public:
         
    
        
- int i = 0, j = 0, sum = 0, mini = INT_MAX;
+ int start=0, sum = 0, mini = INT_MAX;
         
-        while (j < nums.size()) {
+       for(int i=0;i<nums.size();i++) {
             
-            while (sum < target && j < nums.size()) 
-                sum += nums[j++];
-            
-            if(sum>=target){
-            
-                while (sum >= target && i < j) 
-                    sum -= nums[i++];
-                
-                mini = min(mini, j - i + 1);
-            }
-        }
+           sum+=nums[i];
+               
+           
+           while(sum>=target){
+                 
+                 mini=min(mini,i-start+1);
+               sum-=nums[start++];
+               
+           }
+       
+           
+       }
+        
         
         
         return mini == INT_MAX ? 0 : mini; 
