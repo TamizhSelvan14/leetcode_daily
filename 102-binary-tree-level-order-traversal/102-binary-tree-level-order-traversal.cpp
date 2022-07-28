0004667854b3d27f6447e5cab5e8e17a64e2ae98
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-28 11:36:31 +0530

    Time: 9 ms (30.37%), Space: 13.5 MB (9.87%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -11,6 +11,21 @@
  */
 class Solution {
 public:
+    
+    //recursive
+    void solve(vector<vector<int>> &ans,TreeNode *root,int level){
+        if(root==NULL) return;
+        
+        if(ans.size()==level) ans.push_back(vector<int>());
+        
+        ans[level].push_back(root->val);
+        
+        solve(ans,root->left,level+1);
+        solve(ans,root->right,level+1);
+        
+    }
+    
+    
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> ans;
         
@@ -18,39 +33,39 @@ public:
         if(root==NULL)
             return ans;
         
-        queue<TreeNode*> q;
-        q.push(root);
+//         queue<TreeNode*> q;
+//         q.push(root);
         
-        while(!q.empty()){
+//         while(!q.empty()){
             
             
-            int size=q.size();
+//             int size=q.size();
            
-            vector<int> temp;
-            for(int i=0;i<size;i++){
-                 TreeNode *node=q.front();
-                  q.pop();
+//             vector<int> temp;
+//             for(int i=0;i<size;i++){
+//                  TreeNode *node=q.front();
+//                   q.pop();
                 
-                temp.push_back(node->val);
+//                 temp.push_back(node->val);
                 
-                if(node->left)
-                    q.push(node->left);
+//                 if(node->left)
+//                     q.push(node->left);
                 
-                if(node->right)
-                    q.push(node->right);
+//                 if(node->right)
+//                     q.push(node->right);
                 
                 
-            }
+//             }
             
             
             
-            ans.push_back(temp);
+//             ans.push_back(temp);
             
-        }
+//         }
         
         
         
-        
+        solve(ans,root,0);
         
         return ans;
     }
