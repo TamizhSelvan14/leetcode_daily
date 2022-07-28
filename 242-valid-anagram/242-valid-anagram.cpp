53e881eef53da9665732db5f56097e89b96f163b
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-07-28 10:39:48 +0530

    Time: 24 ms (19.33%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,29 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         
         
-        unordered_map<char,int> umap;
-        
-        for(int i=0;i<s.size();i++)
-            umap[s[i]]++;
-        
-        
-        for(int i=0;i<t.size();i++)
-        {    if(umap.find(t[i])!=umap.end())
-                umap[t[i]]--;     
-            else
-                return false;
-        }
-       
-        
-        
-        for(auto x:umap){
-            if(x.second>0)
-                return false;
-        }
-        
-        
-        return true;
+        return s==t;
     }
+    
 };
