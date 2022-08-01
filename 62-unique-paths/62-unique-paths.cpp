c8350ce96810deabbd34c779f4b6258bea37e94e
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-01 10:45:36 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[101][101];
+   
     int solve(int i,int j,int rows,int cols){
         
         if(i==0 and j==0)
@@ -10,21 +10,41 @@ public:
         if(i<0 or j<0)
             return 0;
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+        // if(dp[i][j]!=-1)
+        //     return dp[i][j];
         
         int top=solve(i-1,j,rows,cols);
         int left=solve(i,j-1,rows,cols);
         
         
-        return dp[i][j]=top+left;
+        return top+left;
         
     }
     
     
     
     int uniquePaths(int m, int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(m-1,n-1,m,n);
+         int dp[m][n];
+        memset(dp,0,sizeof(dp));
+        // return solve(m-1,n-1,m,n);
+    
+        for(int i=0;i<m;i++)
+            dp[i][0]=1;
+        
+        
+        for(int i=0;i<n;i++)
+            dp[0][i]=1;
+        
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                
+                int top=dp[i-1][j];
+                int left=dp[i][j-1];
+                dp[i][j]=top+left;
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
