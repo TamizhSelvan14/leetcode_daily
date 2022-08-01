2f1e91e8c899e95b9b6a0cd12d8c1590140ca409
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-01 23:35:47 +0530

    Time: 77 ms (76.50%), Space: 21.7 MB (86.47%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,89 +1,72 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<int>>  &grid,int i,int j,int row,int col){
+    void dfs(vector<vector<int>>  &grid,int i,int j){
         
-        if(i<0 || j<0 || i>=row || j>=col ){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() or grid[i][j]==0){
             return ;
          }
         
-        if(grid[i][j]==0)
-            return ;
         
         grid[i][j]=0;
         
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
         
-        dfs(grid,i+1,j,row,col);
-        dfs(grid,i,j+1,row,col);
-        dfs(grid,i-1,j,row,col);
-        dfs(grid,i,j-1,row,col);
         
-        
-        // return (a&&b&&c&&d); 
     }
     
     
     
     int numEnclaves(vector<vector<int>>& grid) {
-        int count=0;
-
+       
         int row=grid.size();
         int col=grid[0].size();
         
-        //for this sum make all the edge having one island to zero at last count the 1s
         
-        // top right direction  -->
+        //traverse boundary if 1 found then do dfs and make them toan other symbol then count at last remaining ones
         
-        for(int i=0;i<col;i++){
-               if(grid[0][i]==1){
-                    dfs(grid,0,i,row,col);
-                    
-                }
-             
-        }
-        
-        // left most side  
         
         for(int i=0;i<row;i++){
-               if(grid[i][0]==1){
-                    dfs(grid,i,0,row,col);
-                    
-                }
-             
-        }
-           for(int i=0;i<row;i++){
-               if(grid[i][col-1]==1){
-                    dfs(grid,i,col-1,row,col);
-                    
-                }
-             
+            
+            //first row
+            if(grid[i][0]==1)
+                dfs(grid,i,0);
+            
+            //last row
+            if(grid[i][col-1]==1)
+                dfs(grid,i,col-1);
+            
+            
         }
         
         
-        //bottom line
+        
+        
         for(int i=0;i<col;i++){
-               if(grid[row-1][i]==1){
-                    dfs(grid,row-1,i,row,col);
-                    
-                }
-             
+            
+            //first col
+            if(grid[0][i]==1)
+                dfs(grid,0,i);
+            
+            //last col
+            if(grid[row-1][i]==1)
+                dfs(grid,row-1,i);
+            
+            
         }
         
-        
-        //right most line
-        
-     
-        
-        
-        for(int i=1;i<row-1;i++){
-            for(int j=1;j<col-1;j++){
+        int count=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                
                 if(grid[i][j]==1)
                     count++;
+                
             }
         }
-        
-        
-        
         
         
         return count;
