3c05505d54524a434d190a8cce3fdc86c4a76b54
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-02 16:37:03 +0530

    Time: 15 ms (80.60%), Space: 9.3 MB (93.34%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -27,6 +27,29 @@ public:
     }
     
     
+    void dfs(int i,int j,vector<vector<int>>& grid){
+        
+        
+        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() ){
+            return ;
+        }
+        
+        if(grid[i][j]==1)
+            return ;
+       
+        
+        grid[i][j]=1;
+        
+        
+        dfs(i+1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
+        
+        
+    }
+    
+    
     
     
     int closedIsland(vector<vector<int>>& grid) {
@@ -35,13 +58,43 @@ public:
         
         int ans=0;
         
+        //method 1 traverse all the boundaries and fill the 0 -> 1 and travese again to count 0 islands
+        
+        //first and last rows
+        for(int i=0;i<rows;i++){
+            
+            if(grid[i][0]==0)
+                dfs(i,0,grid);
+            
+            if(grid[i][cols-1]==0)
+                dfs(i,cols-1,grid);
+                
+                
+        }
+        
+        
+        //first and last rows
+        for(int i=0;i<cols;i++){
+            
+            if(grid[0][i]==0)
+                dfs(0,i,grid);
+            
+            if(grid[rows-1][i]==0)
+                dfs(rows-1,i,grid);
+                
+        }
+        
+        
+        
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 
                 if(grid[i][j]==0){
                     
-                    if(solve(i,j,grid,rows,cols))
-                        ans++;
+//                     if(solve(i,j,grid,rows,cols))
+//                         ans++;
+                    dfs(i,j,grid);
+                    ans++;
                     
                 }    
                 
