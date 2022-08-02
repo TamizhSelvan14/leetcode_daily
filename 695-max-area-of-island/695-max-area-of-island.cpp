0e9ba94aa72d268ed604640f059140cf67314997
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-02 22:11:48 +0530

    Time: 52 ms (10.12%), Space: 27.3 MB (8.94%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,51 +1,88 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<int>>& grid,int i,int j,int row,int col,int &area)
-    {
-       
-        if(i<0 || j<0 || i>=row || j>=col || grid[i][j]==0){
-            return ;
-        }
+    
+    int dfs(int i,int j,int rows,int cols,vector<vector<int>>& grid){
         
+        if(i<0 or j<0 or i>=rows or j>=cols or grid[i][j]==0)
+            return 0;
+        
+        if(grid[i][j]==1)
+            return 1;
         
         grid[i][j]=0;
-           
-              area++;
-            dfs(grid,i-1,j,row,col,area); //up
-            dfs(grid,i+1,j,row,col,area); //down
-            dfs(grid,i,j+1,row,col,area);  //right
-            dfs(grid,i,j-1,row,col,area); //left
-           
         
-        return;
+        
+      int a=dfs(i+1,j,rows,cols,grid);
+        
+       int b= dfs(i,j+1,rows,cols,grid);
+        
+       int c= dfs(i-1,j,rows,cols,grid);
+        
+        int d=dfs(i,j-1,rows,cols,grid);
+        
+        return 1+(a+b+c+d);  
+    }
+        
+    
+    int bfs(int i,int j,int rows,int cols,vector<vector<int>>& grid){
+        
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        
+        int count=0;
+        while(!q.empty())
+        {
+            int a=q.front().first;
+            int b=q.front().second;
+            
+            q.pop();
+            
+            
+        if(a<0 or b<0 or a>=rows or b>=cols or grid[a][b]==0)
+            continue;
+        
+        if(grid[a][b]==1)
+           count++;
+            
+        grid[a][b]=0;
+            
+            
+         q.push({a+1,b});
+         q.push({a,b+1});
+         q.push({a-1,b});
+         q.push({a,b-1});
+            
+        }
+        return count;
+        
     }
     
     
+        
+        
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         
-        int row=grid.size(),col=grid[0].size() ;
-        int area=0;
-        int maxi=0;
-        for(int i=0;i<row;i++){
-            
-            for(int j=0;j<col;j++){
-                
+        int rows=grid.size();
+        int cols=grid[0].size();
+        
+        int maxi=INT_MIN;
+         int ans=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 
                 if(grid[i][j]==1){
-                    dfs(grid,i,j,row,col,area);
+                   
+                    ans=bfs(i,j,rows,cols,grid);
+                   
                     
-                   maxi=max(maxi,area);
-                    area=0;
                 }
-                
+                 
+                    maxi=max(maxi,ans);
                 
             }
-            
-            
         }
-        
-        
         return maxi;
+        
     }
 };
