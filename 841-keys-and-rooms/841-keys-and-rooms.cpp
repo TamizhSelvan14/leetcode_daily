7988ff4c279108a96a105bae01dbb488508f5398
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-02 21:14:32 +0530

    Time: 20 ms (29.48%), Space: 10.5 MB (34.05%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,35 +1,50 @@
 class Solution {
 public:
+    
+    void dfs(vector<vector<int>> &rooms,vector<int> &visited,int src){
+        
+        visited[src]=1;
+        
+        
+        for(auto x:rooms[src]){
+            
+            if(visited[x]!=1){
+                visited[x]=1;
+                dfs(rooms,visited,x);
+            }
+        }
+        
+    }
+    
+    
+    
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         vector<int> visited(rooms.size(),0);
         
         
-        queue<int> q;
+//         queue<int> q;
         
-        q.push(0);
+//         q.push(0);
         
-            visited[0]=1;
-        while(!q.empty()){
+//         visited[0]=1;
+//         while(!q.empty()){
             
-            int node=q.front();
-            q.pop();
+//             int node=q.front();
+//             q.pop();
             
             
-            for(auto x:rooms[node])
-            {
+//             for(auto x:rooms[node])
+//             {
                 
-                if(visited[x]!=1){
-                    q.push(x);
-                    visited[x]=1;
-                }
-                        
-                
-                
-            }
-            
-            
-            
-        }
+//                 if(visited[x]!=1){
+//                     q.push(x);
+//                     visited[x]=1;
+//                 }
+//             }
+//         }
+        
+       
+        dfs(rooms,visited,0);
         
         for(int i=0;i<visited.size();i++)
           if(visited[i]==0)
