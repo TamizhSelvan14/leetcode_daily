633d2367d2ae0a3c5c818d5ec8ba088c1fd1eabc
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-03 15:32:28 +0530

    Time: 30 ms (5.84%), Space: 14.3 MB (25.31%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,34 +1,56 @@
 class Solution {
 public:
-    void solve(vector<vector<int>> &image,int i,int j,int sourceColor,int colorChange){
+    void dfs(vector<vector<int>> &image,int i,int j,int sourceColor,int colorChange){
         
         
         if(i<0 or j<0 or i>=image.size() or j>=image[0].size() or image[i][j]!=sourceColor or image[i][j]==colorChange){
             return;
         }
         
-        
-       
-        
-        
           image[i][j]=colorChange;
         
         
         //directions
-        solve(image,i+1,j,sourceColor,colorChange);
+        dfs(image,i+1,j,sourceColor,colorChange);
         
-        solve(image,i,j+1,sourceColor,colorChange);
+        dfs(image,i,j+1,sourceColor,colorChange);
         
-        solve(image,i-1,j,sourceColor,colorChange);
-        
-        solve(image,i,j-1,sourceColor,colorChange);
+        dfs(image,i-1,j,sourceColor,colorChange);
+
+        dfs(image,i,j-1,sourceColor,colorChange);
      
         
         
     }
     
     
-    
+    void bfs(vector<vector<int>> &image,int i,int j,int sourceColor,int colorChange){
+        
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        
+        
+        
+        while(!q.empty()){
+            
+            int a=q.front().first;
+            int b=q.front().second;
+            q.pop();
+            
+            if(a<0 or b<0 or a>=image.size() or b>=image[0].size() or image[a][b]!=sourceColor or image[a][b]==colorChange){
+            continue;
+        }
+            
+            image[a][b]=colorChange;
+            
+            q.push({a+1,b});
+            q.push({a,b+1});
+            q.push({a-1,b});
+            q.push({a,b-1});
+            
+            
+        }
+    }
     
     
     
@@ -40,8 +62,8 @@ public:
         
         
         
-        solve(image,sr,sc,sourceColor,color);
-        
+        // dfs(image,sr,sc,sourceColor,color);
+        bfs(image,sr,sc,sourceColor,color);
         
         return image;
     }
