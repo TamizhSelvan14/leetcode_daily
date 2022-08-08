e19932b05fb33ab05b6c074c2d39f04dc7732faa
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-08 15:05:40 +0530

    Time: 237 ms (30.27%), Space: 99.5 MB (5.27%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,44 @@
 class Solution {
 public:
+    
+    int solve(int i,vector<int> &prices,int maxProfit,int minsofar){
+       
+       
+        if(i<0 or i>=prices.size())
+        {
+            return maxProfit;
+        }
+          
+        
+        int currProfit=prices[i]-minsofar;
+        
+        maxProfit=max(maxProfit,currProfit);
+        minsofar=min(minsofar,prices[i]);
+        
+        
+        
+        
+        
+        return maxProfit=max(maxProfit,solve(i+1,prices,maxProfit,minsofar));
+        
+    }
+    
+    
     int maxProfit(vector<int>& prices) {
      
-    int buy=INT_MAX;
-        int sell=0;
+        int n=prices.size();
         
-        for(int i=0;i<prices.size();i++){
-            buy=min(buy,prices[i]);
-            sell=max(sell,prices[i]-buy);
-        }
+        return solve(0,prices,0,prices[0]);
         
-        return sell;
+        
+//     int buy=INT_MAX;
+//         int sell=0;
+        
+//         for(int i=0;i<prices.size();i++){
+//             buy=min(buy,prices[i]);
+//             sell=max(sell,prices[i]-buy);
+//         }
+        
+//         return sell;
     }
 };
