58f4830297913d6a9efaf11262a6d866d0633531
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-08 16:57:22 +0530

    Time: 15 ms (35.49%), Space: 13.1 MB (26.56%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    
+    int solve(int i,int &maxi,int &mini,vector<int>& prices,int &ans){
         
-        int n=prices.size();
+        if(i>=prices.size()){
+            return ans;    
+        }
+       
         
-        int maxi=prices[0];
-        int mini=prices[0];
-        int ans=0;
-        for(int i=1;i<n;i++){
-            
             if(prices[i]<mini){
                 mini=prices[i];
                 maxi=0;
-               
             }
             
             if(prices[i]>maxi){
@@ -22,13 +20,60 @@ public:
                 mini=maxi;
                  maxi=0;
             }
+        
+        
+        
+        solve(i+1,maxi,mini,prices,ans);
+        
+        
+        return ans;
+        
+    }
+    
+    
+    
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size();
+        int ans=0;
+        
+        // return solve(0,prices[0],prices[0],prices,ans);
+        
+//         int maxi=prices[0];
+//         int mini=prices[0];
+//         int ans=0;
+//         for(int i=1;i<n;i++){
+            
+//             if(prices[i]<mini){
+//                 mini=prices[i];
+//                 maxi=0;
+//             }
+            
+//             if(prices[i]>maxi){
+//                 maxi=prices[i];
+//                 ans+=maxi-mini;
+               
+//                 mini=maxi;
+//                  maxi=0;
+//             }
             
            
             
             
+//         }
+        
+//         return ans;
+        
+        
+        
+        int maxi=0;
+        //simple
+        for(int i=1;i<n;i++){
+            
+            ans+=max(maxi,prices[i]-prices[i-1]);
+            
         }
         
         return ans;
-        
     }
 };
