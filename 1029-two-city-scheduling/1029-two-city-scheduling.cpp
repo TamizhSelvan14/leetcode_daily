4892336c201a740120a9213bf3d0f6be55515e8e
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-19 22:57:59 +0530

    Time: 27 ms (7.72%), Space: 12.1 MB (6.20%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -27,12 +27,48 @@ public:
     }
     
     
+    static bool comp(vector<int> &a,vector<int> &b){
+        
+        int profit1=a[1]-a[0];
+        int profit2=b[1]-b[0];
+        
+        return profit1>profit2;
+        
+        
+    }
+    
+    
     
     int twoCitySchedCost(vector<vector<int>>& costs) {
         
-        memset(dp,-1,sizeof(dp));
+//         memset(dp,-1,sizeof(dp));
         
-       return solve(costs,0,0);
+//        return solve(costs,0,0);
+        
+        
+        //divide first half and second half in a way such that makes profit if we reduce
+        
+        // add sort on based on 
+        //     if arr[i][1]-arr[i][0] >0 ->first half na dsecond half add
+        
+        
+        sort(costs.begin(),costs.end(),comp);
+             
+        
+        // for(auto x:costs)
+        //     cout<<x[0]<<" "<<x[1]<<endl;
+        
+            int ans=0;  
+        for(int i=0;i<costs.size()/2;i++){
+            ans+=costs[i][0];
+        }
+        
+        
+        for(int i=costs.size()/2;i<costs.size();i++){
+            ans+=costs[i][1];
+        }
+                
+        return ans;
         
     }
 };
