f23badea95ec5ad1064f8807a2feb692d48d923a
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2022-08-22 22:48:27 +0530

    Time: 135 ms (59.81%), Space: 19.9 MB (89.42%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,53 +1,60 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-            
+        
+        sort(nums.begin(),nums.end());
         
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
         
-        for(int i=0;i<n-2;i++){
-        
-        int low=i+1;
-        int high=n-1;
-        
-        // to make 3 number ==0 we need 2 sum - one negative gives zero
-        int sum=0-nums[i];
+        for(int i=0;i<nums.size()-2;i++){
             
-       if(i==0 || i>0 and nums[i]!=nums[i-1]){     
             
-            while(low<high){
+         if(i>0 and nums[i]==nums[i-1] ){ 
+         continue;
+             
+         }  
+           
+             int n=i+1;
+            int m=nums.size()-1;
+            
+            while(n<m){
+                if(nums[i]>0)
+                    break;
+                
+                int val=nums[i]+nums[n]+nums[m];
+                
+                if(val==0){
                     
-                if(nums[low]+nums[high]==sum){
-                   
-                    ans.push_back({nums[i],nums[low],nums[high]});
-                 
+                    ans.push_back({nums[i],nums[n],nums[m]});
                     
-                    // after pushing we need to remove the duplicates
                     
-                        while(low<high and nums[low]==nums[low+1])
-                            low++;
-
-                        while(low<high and nums[high]==nums[high-1])
-                            high--;
-
-
-                        low++;
-                        high--;
-
-                }else if(nums[low]+nums[high]<sum){
-                    low++;
+                    
+                    n++;
+                    m--;
+                    
+                  
+                    
+                    //remove duplicate
+                    
+                    while(n<m and nums[n]==nums[n-1])
+                        n++;
+                    
+                    
+                    while(n<m and nums[m]==nums[m+1])
+                        m--;
+                    
+                    
+                    
+                }else if(val>0){
+                    m--;
                 }else{
-                    high--;
+                    n++;
                 }
                 
+                
             }
-            
-            
+        
         }
-    }
-            
         return ans;
     }
 };
