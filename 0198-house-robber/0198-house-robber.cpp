b56894efffc704a64ee56ba5f9e8d46c2bb84cb9
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2024-01-18 08:31:59 +0530

    Time: 2 ms (40.49%), Space: 8.2 MB (40.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,65 +1,30 @@
 class Solution {
 public:
-    int solve(vector<int> &nums,int i,  vector<int> &dp)
-    {
-        
+    int solve(vector<int>& nums,int i,vector<int> &dp){
         if(i>=nums.size())
             return 0;
+        
+        // if(i==nums.size()-1){
+        //     return 1;
+        // }
         
         if(dp[i]!=-1)
             return dp[i];
         
         
         int pick=nums[i]+solve(nums,i+2,dp);
-        int skip=solve(nums,i+1,dp);
+        int dont=solve(nums,i+1,dp);
+            
+        return dp[i]=max(pick,dont);
         
         
-        return dp[i]=max(pick,skip);
-        
-        
-        
-    }    
+    }
     
     
     int rob(vector<int>& nums) {
-        // vector<int> dp(nums.size(),-1);
+        vector<int> dp(101,-1);
+       return solve(nums,0,dp);
         
-        
-        
-        
-        // return solve(nums,0,dp);
-        
-        
-        //tabulation
-        
-        if(nums.size()<=2){
-        
-            if (nums.size()==1){
-                return nums[0];
-            }else{
-                return max(nums[0],nums[1]);
-            }
-        }
-        
-        
-         vector<int> dp(nums.size(),0);
-        
-        dp[0]=nums[0];
-
-        for(int i=1;i<nums.size();i++){
-            int pick=nums[i];
-                
-                if(i>1)
-                pick+=dp[i-2];
-            
-            int skip=dp[i-1];
-        
-        
-                dp[i]=max(pick,skip);
-            cout<<dp[i]<<endl;
-        }
-        
-        return dp[nums.size()-1];
         
     }
 };
