3793a0d845fea12c37ed0cca485e4e837620bc6a
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2024-01-18 08:38:44 +0530

    Time: 4 ms (30.04%), Space: 8.4 MB (13.56%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    
-    int solve(vector<int> &nums,int i, vector<int> &dp){
-        
+    int solve(vector<int>& nums,int i, vector<int> &dp){
         if(i>=nums.size())
             return 0;
         
         if(dp[i]!=-1)
             return dp[i];
         
-        
         int pick=nums[i]+solve(nums,i+2,dp);
         int dont=solve(nums,i+1,dp);
+
         
-        
-        return dp[i]= max(pick,dont);
-        
-        
+        return dp[i]=max(pick,dont);
     }
     
-    
-    
-    
     int rob(vector<int>& nums) {
-        
-        if(nums.size()==1)
+         if(nums.size()==1)
             return nums[0];
         
-        
-        vector<int> arr1(nums.begin()+1,nums.end());
-        vector<int> arr2(nums.begin(),nums.end()-1);
+        vector<int> first={nums.begin(),nums.end()-1};
+        vector<int> second={nums.begin()+1,nums.end()};
+    
+        vector<int> dp1(nums.size(),-1);
+        vector<int> dp2(nums.size(),-1);
 
-        vector<int> dp(nums.size()-1,-1);
-        
-         int first= solve(arr1,0,dp);
-          fill(dp.begin(),dp.end(),-1);
-           int second=solve(arr2,0,dp);
-            
-        
-        
-        return max(first,second);
+        return max(solve(first,0,dp1) ,solve(second,0,dp2) );
     }
 };
