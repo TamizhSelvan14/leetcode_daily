3fc0d7815b2bfd3053c5aa6ab7a1d790df4663dc
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2024-02-21 13:22:41 +0530

    Time: 5 ms (47.83%), Space: 14 MB (17.73%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,34 +1,51 @@
 class Solution {
 public:
-  void solve(vector<vector<int>> &ans,vector<int>ds,vector<int>& candidates,int target,int index){
+    void solve( vector<vector<int>> &ans,vector<int> ds,vector<int>& candidates,int target,int index){
         
-        if(target==0 )
+        
+        if(target==0){
             ans.push_back(ds);
-
+            return;
+        }
         
         for(int i=index;i<candidates.size();i++){
-             if(i>index and candidates[i]==candidates[i-1])
+            
+            //avoid negative
+            if (candidates[i]>target)
                 continue;
             
-            if(candidates[i]>target)
+            //avoid duplicates
+            if(i> index && candidates[i]==candidates[i-1])
                 continue;
+            
             
             ds.push_back(candidates[i]);
             solve(ans,ds,candidates,target-candidates[i],i+1);
             ds.pop_back();
+            
+            
+            
+            
         }
+        
+        
+        
         
     }
     
+    
+    
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
         
-        vector<int> ds;
-        
-        //sort it for ans
         sort(candidates.begin(),candidates.end());
         
-        solve(ans,ds,candidates,target,0);
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(ans,ds,candidates, target,0);
+        
+        
+        
+        
         
         
         return ans;
