dd2032bf3ec16c14f2631f3c96ca7ebeb0eb773e
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2024-05-27 18:32:32 +0530

    Time: 48 ms (41.85%), Space: 50.8 MB (49.51%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int curr=nums[0];
+        
+        for(int i=1;i<nums.size();i++){
+            
+            
+            if(curr<=0)
+                return false;
+            
+            
+            curr--;
+            curr=max(curr,nums[i]);
+            
+            
+            
+        }
+        
+        
+        
+        return true;
+        
+    }
+};
