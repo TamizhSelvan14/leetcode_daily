0c21fadb178bf46c01c1c529ac2cdad913723f30
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2025-03-30 08:22:09 +0530

    Time: 0 ms (100%), Space: 9.1 MB (26.99%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -3,15 +3,9 @@ public:
     vector<int> partitionLabels(string s) {
         vector<int> ans;
         //note first and last position of the map
-        unordered_map<char,int> umap1;
         unordered_map<char,int> umap2;
 
     for(int i=0;i<s.size();i++){
-
-        if(umap1.find(s[i])!=umap1.end()){
-            umap1[s[i]]=i;
-        }
-
         umap2[s[i]]=i;
     }
 
@@ -22,10 +16,6 @@ public:
     while(start<s.size()){
         int maxi=-1;
         int end=umap2[s[start]];
-        cout<<"start "<<" "<<start<<endl;
-        cout<<"end "<<" "<<end<<endl;
-
-
         for(int i=start;i<end;i++){
 
             if(umap2.find(s[i])!=umap2.end() && umap2[s[i]]>end){
@@ -38,10 +28,7 @@ public:
         maxi=(end-start)+1;
         start=end+1;
 
-    cout<<"starting"<<" "<<start<<endl;
-        cout<<"ending"<<" "<<end<<endl;
-
-ans.push_back(maxi);
+        ans.push_back(maxi);
 
     }
 
