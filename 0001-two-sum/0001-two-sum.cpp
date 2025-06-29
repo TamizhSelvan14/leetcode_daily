2c1873706ff5db69db542f234a40f01dca180180
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2025-06-29 14:24:28 +0530

    Time: 3 ms (71.02%), Space: 15.9 MB (9.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        vector<int> ans;
-        
+        //hash map store the value and index
         unordered_map<int,int> umap;
-        
         for(int i=0;i<nums.size();i++){
-            
-            if(umap.find(target-nums[i])!=umap.end()){
+                umap[nums[i]]=i;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            if(umap.find(target-nums[i])!=umap.end() && i !=umap[target-nums[i]]){
+                
                 return {i,umap[target-nums[i]]};
             }
-            umap[nums[i]]=i;
-            
         }
-        
-        
-        return ans;
+
+
+
+
+return {};
     }
 };
