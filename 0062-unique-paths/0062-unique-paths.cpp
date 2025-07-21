93ced81a0e12ff5482180cab2146b875f9c67ffb
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2025-07-22 00:40:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.5%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -29,7 +29,19 @@ public:
     }
 
     int uniquePaths(int m, int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(m,n,0,0);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(m,n,0,0);
+
+        //tabulation
+
+        vector<vector<int>> dp(m,vector<int>(n,1));
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
+
+    return dp[m-1][n-1];
     }
 };
