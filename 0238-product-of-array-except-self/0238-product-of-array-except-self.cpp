e8599a54e19f15a6a1ec5a699e0cf21bba3e54da
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2025-09-01 18:20:42 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
-//    1   2   6   24
-//    24  24  12   4
-       
-//    24  12   8   6
-    
-    
-//     -1. -1   0    0   0
-//     0.   0    0   -9   3
-        
-//      0   0.   9.  0.   0
-       
-       
-    
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int> prefix(n);
-        vector<int> suffix(n);
+        vector<int> pre(n);
+        vector<int> suff(n);
         vector<int> ans(n);
-        int pre=1,suf=1;
+
+        int preSum=1;
+        int sufSum=1;
+
         int j=n-1;
+
         for(int i=0;i<n;i++){
-            pre*=nums[i];
-            
-            prefix[i]=pre;
-            
-            suf*=nums[j];
-            
-            suffix[j]=suf;
+            preSum*=nums[i];
+
+            pre[i]=preSum;
+
+            sufSum*=nums[j];
+
+            suff[j]=sufSum;
+
             j--;
+
         }
-        
-        ans[0]=suffix[1];
-        ans[n-1]=prefix[n-2];
-        
+
+        ans[0]=suff[0];
+        ans[n-1]=pre[n-2];
+
+
         for(int i=1;i<n-1;i++){
-            ans[i]=prefix[i-1]*suffix[i+1];  
-            
+            ans[i]=pre[i-1] * suff [i+1];
         }
-        
-        
+
         return ans;
-        
     }
 };
