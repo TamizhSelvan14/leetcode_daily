485bf33e808d0669e34fb4776a194f3e5313552f
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2025-09-03 15:00:14 -0700

    Time: 51 ms (50.17%), Space: 29.2 MB (37.92%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,53 +2,45 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        
         sort(nums.begin(),nums.end());
-        
-        
-        
-        for(int i=0;i<nums.size()-2;i++){
-            
-           
-            int j=i+1;
-            int k=nums.size()-1;
-            
-             if(i>0 and nums[i]==nums[i-1])
-               continue;
-            
-            while(j<k){
+        int n=nums.size();
+        for(int i=0;i<n-2;i++){
+
+            int start=i+1;
+            int end=n-1;
+
+            if(i>0 and nums[i]==nums[i-1])   //duplciates
+                continue;
+
+
+            while(start<end){
+                if(nums[i]+nums[start]+nums[end]==0){
+                    ans.push_back({nums[i],nums[start],nums[end]});
                 
-                if(nums[i]+nums[j]+nums[k]==0){
-                    ans.push_back({nums[i],nums[j],nums[k]});
-                
-                j++;k--;
-                
-                //skip duplicates
-                while(j<k and nums[j]==nums[j-1]){
-                    j++;
-                }
-                
-                //skip duplicates
-                while(j<k and nums[k]==nums[k+1]){
-                    k--;
-                }
-                
-                
-                }else if(nums[i]+nums[j]+nums[k]<0){
-                    j++;
+                start++;end--;
+                //duplicates
+                while(start<end and nums[start]==nums[start-1])
+                        start++;
+
+                while(start<end and nums[end]==nums[end+1])
+                        end--;
+
+                }else if(nums[i]+nums[start]+nums[end]>0){
+                    end--;
                 }else{
-                    k--;
+                    start++;
                 }
-                
+
+
+
+
+
             }
-            
-            
-            
-            
+
         }
-        
-        
-        return ans;
-        
+
+
+
+    return ans;
     }
 };
