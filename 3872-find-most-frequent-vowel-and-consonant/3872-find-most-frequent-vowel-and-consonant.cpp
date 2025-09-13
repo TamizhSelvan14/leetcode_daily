17c960a208b550142b4be5ac08805e73d6e374d6
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2025-09-13 00:59:28 -0700

    Time: 0 ms (100%), Space: 9.8 MB (44.54%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool isVowel(char c)
-    {
-        return (c=='a' or c=='e' or c=='i' or c=='o' or c=='u' );
-    }
+    // bool isVowel(char c)
+    // {
+    //     return (c=='a' or c=='e' or c=='i' or c=='o' or c=='u' );
+    // }
 
 
     int maxFreqSum(string s) {
@@ -11,12 +11,12 @@ public:
         int maxiV=0;
         int maxiC=0;
 
-        for(auto x:s){
-             umap[x]++;
-            if(isVowel(x)){
-                maxiV=max(maxiV,umap[x]);
+        for(auto c:s){
+             umap[c]++;
+            if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u' ){
+                maxiV=max(maxiV,umap[c]);
             }else{
-                maxiC=max(maxiC,umap[x]);
+                maxiC=max(maxiC,umap[c]);
             }
 
         }
