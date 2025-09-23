1ffef32616f5c6aaf0be116df84ab0db02ebcda9
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2025-09-22 21:22:34 -0700

    Time: 0 ms (100%), Space: 8.1 MB (87.43%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        
+        int i=0,j=0;
+          int v1=0;
+            int v2=0;
+
+
+        while(i<version1.size() or j<version2.size()){
+
+            //calculate the segments 
+
+            v1=0;
+            v2=0;
+
+            while(i< version1.size() and version1[i]!='.'){
+                v1=v1*10 +(version1[i]-'0');
+                i++;
+            }
+
+             while(j < version2.size() and version2[j]!='.'){
+                v2=v2*10 +(version2[j]-'0');
+                j++;
+            }
+
+                if(v1<v2)
+                return -1;
+                else if(v1>v2)
+                return 1;
+
+                //skip .
+                i++;j++;
+
+        }
+
+
+        return 0;
+    }
+};
