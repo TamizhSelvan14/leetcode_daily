e83aa7a8787dff8035fa74e6e824b124d111905d
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2025-09-25 01:06:02 -0700

    Time: 4 ms (7.22%), Space: 23.5 MB (5.06%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,59 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& triangle,int i,int j, vector<vector<int>> dp){
-
-        if(i>=triangle.size() or j>=triangle[i].size())
-        return 0;
-
+    vector<vector<int>> dp{201,vector<int>(201,0)};
+    int solve(int i,int j,vector<vector<int>>& triangle){
+        
+        if(j>triangle[i].size()-1)
+            return 1e9;
+        
+        if(i==triangle.size()-1)
+            return triangle[i][j];
+        
         if(dp[i][j]!=-1)
         return dp[i][j];
-     
-        int down=triangle[i][j]+solve(triangle,i+1,j,dp);
-
-        int rightDiagonal=triangle[i][j]+solve(triangle,i+1,j+1,dp);
-
+        
+        int down=triangle[i][j] +solve(i+1,j,triangle);
+        int rightDiagonal=triangle[i][j]+solve(i+1,j+1,triangle);
+        
         return dp[i][j]=min(down,rightDiagonal);
+        
+        
+        
     }
-
-
+    
     int minimumTotal(vector<vector<int>>& triangle) {
-                vector<vector<int>> dp(triangle.size(),vector<int>(triangle.size(),-1));    
-
-      return solve(triangle,0,0,dp);  
+        
+        
+        // return solve(0,0,triangle);
+        int mini=INT_MAX;
+        
+        int rows=triangle.size();
+        
+        dp[0][0]=triangle[0][0];
+        
+        for(int i=1;i<rows;i++){
+            
+            for(int j=0;j<triangle[i].size();j++){
+                
+                if(j==0 )
+                    dp[i][j]=triangle[i][j]+dp[i-1][j];
+                else if(j==triangle[i].size()-1)
+                        dp[i][j]=triangle[i][j]+dp[i-1][j-1];
+                else {
+                    dp[i][j]=triangle[i][j]+min(dp[i-1][j],dp[i-1][j-1]);
+                    
+                }
+                
+               
+                
+                // cout<<dp[i][j]<<" ";
+            }
+            // cout<<endl;
+            
+        }
+        
+        
+        
+        return *min_element(dp[rows-1].begin(),dp[rows-1].begin()+triangle[rows-1].size());
     }
 };
