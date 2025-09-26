87e6cd3ec533f11c18c151e028d11959c1f7ff02
Tamizh selvan <59242527+TamizhSelvan14@users.noreply.github.com>
2025-09-25 17:17:40 -0700

    Time: 7 ms (11.33%), Space: 28.1 MB (86.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,18 +3,34 @@ public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
 
+        if(n==1)
+        return nums[0];
+
         int cap=n/2;
-        unordered_map<int,int> umap;
-        for(auto x:nums){
-            umap[x]++;
+                    cout<<" cap "<<cap<<endl;
+
+        int i=0;
+    sort(nums.begin(),nums.end());
+
+        while(i<n){
+
+            int tempCount=1;
+            cout<<" i "<<nums[i]<<endl;
+
+            i++;
+            while(i<n and nums[i-1]==nums[i]){
+                tempCount++;
+                if(tempCount>cap)
+                   return nums[i];
+                i++;
+            }
+                    cout<<" tempCount "<<tempCount<<endl;
+
+            
+
+
         }
 
-        for(auto x:umap)
-        {
-                if(x.second>cap){
-                    return x.first;
-                }
-        }
-        return -1;
+        return 0;
     }
 };
